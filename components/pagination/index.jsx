f6871e89f589ad0be43bf7a8c935da66e6fd56d0
Mai Tri Toan <69956369+likeoneringfake@users.noreply.github.com>
2022-06-13 12:37:24 +0700

    Rebuild Pagination Components

--- components/pagination/index.jsx
+++ components/pagination/index.jsx
@@ -1,41 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-const Pagination = ({totalPage}) => {
+const Pagination = ({_totalPage, _currentPage, _pageShowLength = 5}) => {
 
 	const route = useRouter()
 
-	const maxPage = totalPage > 0 ? totalPage : 1
+	const maxPage = parseInt(_totalPage)  > 0 ? parseInt(_totalPage) : 1
 	
-	const pageShowLength = 5
 
 	const page = [...Array(maxPage)].map( (_, i) => i + 1)
 
 	const [pageShow, setPageShow] = useState([])
 
-	const currentPage = /^[0-9]*$/.test(parseInt(route.query._page))
+	const currentPage = /^[0-9]*$/.test(_currentPage ?? parseInt(route.query._page))
 		? parseInt(route.query._page) < 1
 			? 1
 			: parseInt(route.query._page) > maxPage
 			? maxPage
 			: parseInt(route.query._page)
 		: 1
-	
-	useEffect(() => {
-		if (currentPage === 1 || currentPage === 2) {
-			setPageShow(page.slice(0, pageShowLength))
-		} else if (currentPage > 2) {
-			if (currentPage === maxPage - 1) {
-				setPageShow(page.slice(currentPage - (5 - 1), currentPage + 2))
-			} else if (currentPage === maxPage) {
-				setPageShow(page.slice(currentPage - 5, currentPage + 2))
-			} else {
-				setPageShow(page.slice(currentPage - (5 - 2), currentPage + 2))
-			}
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [currentPage])
 
-	
+	useEffect(() => {
+		const prev_nextPage = Math.floor(_pageShowLength / 2)
+
+		if(currentPage > 0 && currentPage <= (_pageShowLength - prev_nextPage)) 
+			return setPageShow(page.slice(0, _pageShowLength))
+		
+		if(currentPage >= (maxPage - prev_nextPage)) 
+			return setPageShow(page.slice(maxPage - _pageShowLength, maxPage))
+
+		return setPageShow(page.slice(currentPage - (_pageShowLength - prev_nextPage), currentPage + prev_nextPage))
+		
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentPage, _totalPage])
+
 	const goToPage = (currentPage) => {
 		const queryObj = {...route.query, _page: (currentPage)}
 		return	route.push(`?${new URLSearchParams(queryObj).toString()}`)
@@ -46,7 +43,7 @@ const Pagination = ({totalPage}) => {
 			goToPage(currentPage + 1)
 		}
 	}
-	const prePage = () => {
+	const prevPage = () => {
 		if (currentPage > 1) {			
 			goToPage(currentPage - 1)
 		}
@@ -90,7 +87,7 @@ const Pagination = ({totalPage}) => {
 						</svg>
 					</button>
 					<button
-						onClick={prePage}
+						onClick={prevPage}
 						className={`w-6 h-8 rounded ${
 							currentPage === 1
 								? 'text-gray-300 dark:text-gray-800'
